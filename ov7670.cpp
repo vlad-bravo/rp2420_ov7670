e54e47a1c5b649f9f22778a480518060117b4064
vlad-bravo <17.bravo@gmail.com>
2026-04-22 17:09:48 +0500

    interupt based bw and color

--- ov7670.cpp
+++ ov7670.cpp
@@ -12,8 +12,7 @@
 #define VSYNC_PIN       23
 #define DATA_BASE_PIN   16
 
-#define TEST0_PIN       29
-#define TEST1_PIN       4
+#define TEST0_PIN       14
 #define LED_PIN         25
 #define WS2812_PIN      23
 #define USERKEY_PIN     24
@@ -26,27 +25,41 @@
 // --- Буферы и указатели ---
 uint8_t buffer1[640];
 uint8_t buffer2[640];
-uint8_t preamble[4]; // 0 - marker, 1 - packet type, 2 - msb, 3 - lsb
+uint8_t buffer3[640];
+uint8_t buffer4[640];
+uint8_t preamble[5]; // 0 - marker, 1 - packet type, 2 - msb, 3 - lsb, 4 - chunk#
 
 uint8_t* capture_buf = buffer1; // Указатель для накопления
 uint8_t* process_buf = buffer2; // Указатель для обработки
+uint8_t* capture_c_buf = buffer3; // Указатель для накопления
+uint8_t* process_c_buf = buffer4; // Указатель для обработки
 uint8_t* temp_buf;              // Временный указатель
 
 // --- Переменные состояния ---
 volatile uint16_t x, y, process_y;
+volatile bool line_flag = false, frame_flag = false;
 
 // Глобальная переменная для подсчета кадров
 static uint8_t frame_counter = 0;
 
 // Вызывается после получения каждой строки
 void process_line() {
-    for (uint8_t i = 6; i<17; i++) // Цикл по 22 кусочкам строки
+    for (uint8_t i = 4; i<18; i++) // Цикл по 22 кусочкам строки
     {
         preamble[1] = '0' + i;
         fwrite(&preamble[0], 4, 1, stdout);
         fwrite(&process_buf[i*29], 29, 1, stdout);
         fflush(stdout);
     }
+    // for (uint8_t i = 0; i<22; i++) // Цикл по 44 кусочкам строки
+    // {
+    //     preamble[1] = 'P';
+    //     preamble[4] = i;
+    //     fwrite(&preamble[0], 5, 1, stdout);
+    //     fwrite(&process_buf[i*14], 14, 1, stdout);
+    //     fwrite(&process_c_buf[i*14], 14, 1, stdout);
+    //     fflush(stdout);
+    // }
 }
 
 // Вызывается после получения всех 480 строк кадра
@@ -106,6 +119,69 @@ void init_xclk() {
     pwm_set_enabled(slice, true);
 }
 
+// Interrupt handler function - note the required parameter types
+void gpio_callback(uint gpio, uint32_t events) {
+    if (gpio == PCLK_PIN && (events & GPIO_IRQ_EDGE_RISE)) {
+        if (gpio_get(VSYNC_PIN) != 0)
+        {
+            if (y>0)
+            {
+                y = 0;
+                frame_flag = true;
+            }
+        }
+        else
+        {
+            if (gpio_get(HREF_PIN) != 0)
+            {
+                if (x & 1)
+                    capture_c_buf[x>>1] = (
+                        gpio_get(DATA_BASE_PIN+5)<<7
+                        | gpio_get(DATA_BASE_PIN+4)<<6
+                        | gpio_get(DATA_BASE_PIN+3)<<5
+                        | gpio_get(DATA_BASE_PIN+2)<<4
+                        | gpio_get(DATA_BASE_PIN+1)<<3
+                        | gpio_get(DATA_BASE_PIN)<<2
+                    );
+                else
+                {
+gpio_put(TEST0_PIN, true);
+                    capture_buf[x>>1] = (
+                        gpio_get(DATA_BASE_PIN+5)<<7
+                        | gpio_get(DATA_BASE_PIN+4)<<6
+                        | gpio_get(DATA_BASE_PIN+3)<<5
+                        | gpio_get(DATA_BASE_PIN+2)<<4
+                        | gpio_get(DATA_BASE_PIN+1)<<3
+                        | gpio_get(DATA_BASE_PIN)<<2
+                    );
+gpio_put(TEST0_PIN, false);
+                }
+                x++;
+            }
+            else
+            {
+                if (x>0)
+                {
+                    x = 0;
+                    // Сохранить номер полученной строки
+                    process_y = y;
+                    preamble[2] = y>>8;
+                    preamble[3] = y;
+                    y++;
+                    // Поменять указатели на буферы
+                    temp_buf = capture_buf;
+                    capture_buf = process_buf;
+                    process_buf = temp_buf;
+                    temp_buf = capture_c_buf;
+                    capture_c_buf = process_c_buf;
+                    process_c_buf = temp_buf;
+                    line_flag = true;
+                }
+            }
+        }
+    }
+}
+
 int main() {
     stdio_init_all();
 
@@ -130,17 +206,31 @@ int main() {
     // Инициализация TEST
     gpio_init(TEST0_PIN);
     gpio_set_dir(TEST0_PIN, GPIO_OUT);
-    gpio_init(TEST1_PIN);
-    gpio_set_dir(TEST1_PIN, GPIO_OUT);
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
     preamble[0] = 0xfc;
 
+    // Set up the interrupt - this one function does everything!
+    gpio_set_irq_enabled_with_callback(PCLK_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+
     while (true) {
 
-        while (gpio_get(PCLK_PIN));
-        if (gpio_get(VSYNC_PIN))
+        if (line_flag)
+        {
+            line_flag = false;
+            process_line();
+        };
+
+        if (frame_flag)
+        {
+            frame_flag = false;
+            process_frame();
+        };
+/*
+        while (gpio_get(PCLK_PIN) != 0);
+        sleep_us(1);
+        if (gpio_get(VSYNC_PIN) != 0)
         {
             if (y>0)
             {
@@ -150,20 +240,27 @@ int main() {
         }
         else
         {
-            if (gpio_get(HREF_PIN))
+            if (gpio_get(HREF_PIN) != 0)
             {
-                //gpio_put(TEST1_PIN, true);
                 if (~x & 1)
                     capture_buf[x>>1] = (
                         gpio_get(DATA_BASE_PIN+5)<<7
                         | gpio_get(DATA_BASE_PIN+4)<<6
                         | gpio_get(DATA_BASE_PIN+3)<<5
-                        //| gpio_get(DATA_BASE_PIN+2)<<4
-                        //| gpio_get(DATA_BASE_PIN+1)<<3
-                        //| gpio_get(DATA_BASE_PIN)<<2
+                        | gpio_get(DATA_BASE_PIN+2)<<4
+                        | gpio_get(DATA_BASE_PIN+1)<<3
+                        | gpio_get(DATA_BASE_PIN)<<2
+                    );
+                else
+                    capture_c_buf[x>>1] = (
+                        gpio_get(DATA_BASE_PIN+5)<<7
+                        | gpio_get(DATA_BASE_PIN+4)<<6
+                        | gpio_get(DATA_BASE_PIN+3)<<5
+                        | gpio_get(DATA_BASE_PIN+2)<<4
+                        | gpio_get(DATA_BASE_PIN+1)<<3
+                        | gpio_get(DATA_BASE_PIN)<<2
                     );
                 x++;
-                //gpio_put(TEST1_PIN, false);
             }
             else
             {
@@ -180,11 +277,15 @@ int main() {
                     temp_buf = capture_buf;
                     capture_buf = process_buf;
                     process_buf = temp_buf;
+                    temp_buf = capture_c_buf;
+                    capture_c_buf = process_c_buf;
+                    process_c_buf = temp_buf;
                     process_line();
                     //gpio_put(TEST0_PIN, false);
                 }
             }
         }
         while (gpio_get(PCLK_PIN) == 0);
+*/
     }
 }
